720d7a5189d50a941c6714f3d6759241b5c98984
Marco Pivetta <ocramius@gmail.com>
2014-07-11 08:28:52 +0000

    Importing state as of zendframework/zf2@d69d9de5a314f7004f671b37c2a144a6bb090f08 (1405067332)
    Automatic import via rsync

--- Sql/Platform/Platform.php
+++ Sql/Platform/Platform.php
@@ -36,10 +36,6 @@ class Platform extends AbstractPlatform
                 $platform = new Oracle\Oracle();
                 $this->decorators = $platform->decorators;
                 break;
-            case 'ibm db2':
-            case 'ibmdb2':
-                $platform = new IbmDb2\IbmDb2();
-                $this->decorators = $platform->decorators;
             default:
         }
     }
