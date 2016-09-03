8bd2b484d16670d3063efb153e7b050a115750fc
Sasha Alex Romanenko <alex@alex-tech-adventures.com>
2016-09-03 15:11:23 -0500

    Process indexes separately from constraints, after CreateTable specification.

--- src/Sql/Platform/Postgresql/Ddl/CreateTableDecorator.php
+++ src/Sql/Platform/Postgresql/Ddl/CreateTableDecorator.php
@@ -9,12 +9,17 @@
 
 namespace Zend\Db\Sql\Platform\Postgresql\Ddl;
 
+use Zend\Db\Adapter\Driver\DriverInterface;
+use Zend\Db\Adapter\ParameterContainer;
 use Zend\Db\Sql\Ddl\CreateTable;
+use Zend\Db\Sql\Ddl\Index\Index;
 use Zend\Db\Sql\Platform\PlatformDecoratorInterface;
 use Zend\Db\Adapter\Platform\PlatformInterface;
+use Zend\Db\Sql\Platform\Postgresql\Ddl\Index\IndexDecorator;
 
 class CreateTableDecorator extends CreateTable implements PlatformDecoratorInterface
 {
+    const INDEXES = 'indexes';
 
     /**
      * @var CreateTable
@@ -22,13 +27,90 @@ class CreateTableDecorator extends CreateTable implements PlatformDecoratorInter
     protected $subject;
 
     /**
+     * @var string[]
+     */
+    protected $indexes = [];
+
+    /**
      * @inheritDoc
      */
-    public function setSubject($subject)
-    {
+    protected $indexSpecification = [
+        self::INDEXES => [
+            "\n%1\$s" => [
+                [1 => '%1$s;', 'combinedby' => "\n"]
+            ]
+        ]
+    ];
+
+    /**
+     * @param $subject
+     * @return mixed
+     */
+    public function setSubject($subject) {
         $this->subject = $subject;
+
+        $this->specifications = array_merge($this->specifications, $this->indexSpecification);
+        $this->subject->specifications = $this->specifications;
+
+        return $this;
     }
 
+    /**
+     * @inheritDoc
+     */
+    protected function buildSqlString(
+        PlatformInterface $platform,
+        DriverInterface $driver = null,
+        ParameterContainer $parameterContainer = null
+    ) {
+        $this->separateIndexesFromConstraints();
+
+        return parent::buildSqlString($platform, $driver, $parameterContainer);
+    }
+
+    private function separateIndexesFromConstraints()
+    {
+        // take advantage of PHP's ability to access protected properties of different instances created from same class
+        $this->indexes = array_filter($this->subject->constraints, function($constraint) {
+            return $constraint instanceof Index;
+        });
+
+        $filteredConstraints = array_filter($this->subject->constraints, function($constraint) {
+            return !($constraint instanceof Index);
+        });
+
+        $this->subject->constraints = $filteredConstraints;
+
+        array_walk($this->indexes, function (&$index, $key) {
+            $indexDecorator = new IndexDecorator();
+            $indexDecorator->setSubject($index);
+            $indexDecorator->setTable($this->subject->table);
+            $index = $indexDecorator;
+        });
+    }
+
+    /**
+     * @param PlatformInterface|null $adapterPlatform
+     * @return array|void
+     */
+    protected function processIndexes(PlatformInterface $adapterPlatform = null) {
+        if (!$this->indexes) {
+            return;
+        }
+
+        $sqls = [];
+
+        foreach ($this->indexes as $index) {
+            $sqls[] = $this->processExpression($index, $adapterPlatform);
+        }
+
+        return [$sqls];
+    }
+
+    /**
+     * @param PlatformInterface|null $adapterPlatform
+     * @return array|void
+     */
     protected function processStatementEnd(PlatformInterface $adapterPlatform = null)
     {
         return ["\n);"];
--- test/Sql/Platform/Postgresql/Ddl/CreateTableDecoratorTest.php
+++ test/Sql/Platform/Postgresql/Ddl/CreateTableDecoratorTest.php
@@ -19,7 +19,7 @@ class CreateTableDecoratorTest extends \PHPUnit_Framework_TestCase
     /**
      * @testdox integration test: Testing CreateTableDecorator will use CreateTable as an internal state to adjust Index creation to be a separate statement
      * @covers Zend\Db\Sql\Platform\Postgresql\Ddl\CreateTableDecorator::setSubject
-     * @covers Zend\Db\Sql\Platform\Postgresql\Ddl\CreateTableDecorator::processConstraints
+     * @covers Zend\Db\Sql\Platform\Postgresql\Ddl\CreateTableDecorator::buildSqlString
      * @covers Zend\Db\Sql\Platform\Postgresql\Ddl\CreateTableDecorator::processIndexes
      * @covers Zend\Db\Sql\Platform\Postgresql\Ddl\CreateTableDecorator::processStatementEnd
      * @dataProvider tableDefinitionsProvider
@@ -38,20 +38,52 @@ class CreateTableDecoratorTest extends \PHPUnit_Framework_TestCase
         $id = new Ddl\Column\Integer('id', false, null);
         $name = new Ddl\Column\Varchar('username', false, null);
         $name->setLength(1024);
+        $nameUnique = new Ddl\Constraint\UniqueKey('username');
+
+        $idIndex = new Ddl\Index\Index('id', 'id_idx');
+        $nameIndex = new Ddl\Index\Index('username', 'username_index');
 
 
         $columnsOnly = new CreateTable('columns_only');
         $columnsOnly->addColumn($id);
         $columnsOnly->addColumn($name);
 
-        $expectedColumnsOnly =
-        'CREATE TABLE "columns_only" ( '."\n".
-        '    "id" INTEGER NOT NULL,'."\n".
-        '    "username" VARCHAR(1024) NOT NULL '."\n".
-        ');';
+        $withSingleIndex = new CreateTable('with_single_index');
+        $withSingleIndex->addColumn($id);
+        $withSingleIndex->addColumn($name);
+        $withSingleIndex->addConstraint($idIndex);
+
+        $mixed = new CreateTable('mixed');
+        $mixed->addColumn($id);
+        $mixed->addColumn($name);
+        $mixed->addConstraint($nameUnique);
+        $mixed->addConstraint($idIndex);
+        $mixed->addConstraint($nameIndex);
+
+
+        $expectedColumnsOnly = 'CREATE TABLE "columns_only" ( ' . "\n"
+                             . '    "id" INTEGER NOT NULL,' . "\n"
+                             . '    "username" VARCHAR(1024) NOT NULL ' . "\n"
+                             . ');';
+
+        $expectedWithSingleInstance = 'CREATE TABLE "with_single_index" ( ' . "\n"
+                                    . '    "id" INTEGER NOT NULL,' . "\n"
+                                    . '    "username" VARCHAR(1024) NOT NULL ' . "\n"
+                                    . '); ' . "\n"
+                                    . 'CREATE INDEX "id_idx" ON "with_single_index"("id");';
+
+        $expectedMixed = 'CREATE TABLE "mixed" ( ' . "\n"
+                       . '    "id" INTEGER NOT NULL,' . "\n"
+                       . '    "username" VARCHAR(1024) NOT NULL , ' . "\n"
+                       . '    UNIQUE ("username") ' . "\n"
+                       . '); ' . "\n"
+                       . 'CREATE INDEX "id_idx" ON "mixed"("id");' . "\n"
+                       . 'CREATE INDEX "username_index" ON "mixed"("username");';
 
         return [
-            [$columnsOnly, $expectedColumnsOnly]
+            [$columnsOnly,      $expectedColumnsOnly],
+            [$withSingleIndex,  $expectedWithSingleInstance],
+            [$mixed,            $expectedMixed]
         ];
     }
 
