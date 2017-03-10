2f4e48b75195e5350738b0756f7d13ada5d87f1b
Sasha Alex Romanenko <alex@alex-tech-adventures.com>
2017-03-09 23:21:38 -0500

    #215 Alter Table Support for column adding in SQL Server. Options are processed following syntax at microsoft page.

--- test/Sql/Platform/SqlServer/Ddl/AlterTableDecoratorTest.php
+++ test/Sql/Platform/SqlServer/Ddl/AlterTableDecoratorTest.php
@@ -5,7 +5,7 @@
  *
  * @link      http://github.com/zendframework/zf2 for the canonical source repository
  *
- * @copyright Copyright (c) 2005-2016 Zend Technologies USA Inc. (http://www.zend.com)
+ * @copyright Copyright (c) 2005-2017 Zend Technologies USA Inc. (http://www.zend.com)
  * @license   http://framework.zend.com/license/new-bsd New BSD License
  */
 
@@ -13,7 +13,9 @@ namespace ZendTest\Db\Sql\Platform\SqlServer\Ddl;
 
 use Zend\Db\Sql\Ddl\AlterTable;
 use Zend\Db\Sql\Ddl\Column\Column;
+use Zend\Db\Sql\Ddl\Column\Varbinary;
 use Zend\Db\Sql\Ddl\Constraint\PrimaryKey;
+use Zend\Db\Sql\Exception\InvalidArgumentException;
 use Zend\Db\Sql\Platform\Sqlserver\Ddl\AlterTableDecorator;
 use ZendTest\Db\TestAsset\TrustingSqlServerPlatform;
 
@@ -27,23 +29,85 @@ class AlterTableDecoratorTest extends \PHPUnit_Framework_TestCase
         $platform = new TrustingSqlServerPlatform();
 
         $ctd = new AlterTableDecorator();
-
         $ct = new AlterTable('altered');
 
+        // test for valid syntax, not valid engine semantics
         $id = new Column('id');
         $id->addConstraint(new PrimaryKey());
+        $id->setOption('filestream', true);
+        $id->setOption('collate', 'Cyrillic_General_CI_AS');
+        $id->setOption('rowguidcol', true);
+        $id->setOption('sparse', true);
+        $id->setOption('encrypted with', '(COLUMN_ENCRYPTION_KEY = key_name)');
+        $id->setOption('masked with', "(FUNCTION = ' mask_function ')");
+        $id->setOption('identity', '(1, 1)');
         $ct->addColumn($id);
+
+        $pk = new Column('named_pk');
+        $pk->addConstraint(new PrimaryKey(null, 'specified_pk'));
+        $ct->addColumn($pk);
+
         $this->assertEquals(
             "ALTER TABLE [altered]\n".
-                ' ADD [id] INTEGER NOT NULL PRIMARY KEY',
+                " ADD [id] INTEGER FILESTREAM COLLATE [Cyrillic_General_CI_AS] IDENTITY (1, 1) NOT NULL ROWGUIDCOL SPARSE ENCRYPTED WITH (COLUMN_ENCRYPTION_KEY = key_name) MASKED WITH (FUNCTION = ' mask_function ') PRIMARY KEY;\n".
+            "ALTER TABLE [altered]\n".
+                " ADD [named_pk] INTEGER NOT NULL CONSTRAINT [specified_pk] PRIMARY KEY;",
             $ctd->setSubject($ct)->getSqlString($platform)
         );
 
+        // add constraint without columns
         $ct = new AlterTable('constrained');
         $ct->addConstraint(new PrimaryKey(['u_id', 'g_id'], 'UserGroup_PK'));
         $this->assertEquals(
             "ALTER TABLE [constrained]\n".
-                '    ADD CONSTRAINT [UserGroup_PK] PRIMARY KEY ([u_id], [g_id])',
+                ' ADD CONSTRAINT [UserGroup_PK] PRIMARY KEY ([u_id], [g_id]);',
+            trim($ctd->setSubject($ct)->getSqlString($platform))
+        );
+    }
+
+    public function testIdentityBooleanConvertsToDefaultParams()
+    {
+        $platform = new TrustingSqlServerPlatform();
+
+        $ctd = new AlterTableDecorator();
+        $ct = new AlterTable('identifiable');
+        $id = new Column('id');
+        $id->setOption('identity', true);
+        $ct->addColumn($id);
+
+        $this->assertEquals(
+            "ALTER TABLE [identifiable]\n".
+                ' ADD [id] INTEGER IDENTITY (1, 1) NOT NULL;',
+            $ctd->setSubject($ct)->getSqlString($platform)
+        );
+    }
+
+    public function testIdentityInvalidFormatThrowsException()
+    {
+        $platform = new TrustingSqlServerPlatform();
+
+        $ctd = new AlterTableDecorator();
+        $ct = new AlterTable('invalid');
+
+        $id = new Column('id');
+        $id->setOption('identity', '1');
+        $ct->addColumn($id);
+
+        $this->setExpectedException(InvalidArgumentException::class);
+        $ctd->setSubject($ct)->getSqlString($platform);
+    }
+
+    public function testVarbinarySyntaxCorrected()
+    {
+        $platform = new TrustingSqlServerPlatform();
+
+        $ctd = new AlterTableDecorator();
+        $ct = new AlterTable('hasbinarydata');
+
+        $ct->addColumn(new Varbinary('binary'));
+        $this->assertEquals(
+            "ALTER TABLE [hasbinarydata]\n".
+                ' ADD [binary] VARBINARY (max) NOT NULL;',
             $ctd->setSubject($ct)->getSqlString($platform)
         );
     }
