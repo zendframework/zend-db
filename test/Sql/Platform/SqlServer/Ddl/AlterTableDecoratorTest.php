844e2f4dad7aadc93641fd5a3e085a910e3a7f22
Sasha Alex Romanenko <alex@alex-tech-adventures.com>
2017-03-11 04:10:56 -0500

    style fixes

--- test/Sql/Platform/SqlServer/Ddl/AlterTableDecoratorTest.php
+++ test/Sql/Platform/SqlServer/Ddl/AlterTableDecoratorTest.php
@@ -1,16 +1,13 @@
 <?php
-
 /**
- * Zend Framework (http://framework.zend.com/).
- *
- * @link      http://github.com/zendframework/zf2 for the canonical source repository
- *
- * @copyright Copyright (c) 2005-2017 Zend Technologies USA Inc. (http://www.zend.com)
- * @license   http://framework.zend.com/license/new-bsd New BSD License
+ * @see       http://github.com/zendframework/zend-db for the canonical source repository
+ * @copyright Copyright (c) 2015-2017 Zend Technologies USA Inc. (http://www.zend.com)
+ * @license   http://github.com/zendframework/zend-db/blob/master/LICENSE.md New BSD License
  */
 
 namespace ZendTest\Db\Sql\Platform\SqlServer\Ddl;
 
+use PHPUnit_Framework_TestCase as TestCase;
 use Zend\Db\Sql\Ddl\AlterTable;
 use Zend\Db\Sql\Ddl\Column\Column;
 use Zend\Db\Sql\Ddl\Column\Varbinary;
@@ -19,7 +16,7 @@ use Zend\Db\Sql\Exception\InvalidArgumentException;
 use Zend\Db\Sql\Platform\Sqlserver\Ddl\AlterTableDecorator;
 use ZendTest\Db\TestAsset\TrustingSqlServerPlatform;
 
-class AlterTableDecoratorTest extends \PHPUnit_Framework_TestCase
+class AlterTableDecoratorTest extends TestCase
 {
     /**
      * @covers Zend\Db\Sql\Platform\SqlServer\Ddl\AlterTableDecorator::getSqlString
@@ -28,8 +25,8 @@ class AlterTableDecoratorTest extends \PHPUnit_Framework_TestCase
     {
         $platform = new TrustingSqlServerPlatform();
 
-        $atd = new AlterTableDecorator();
-        $at = new AlterTable('altered');
+        $alterDecorator = new AlterTableDecorator();
+        $alterTable = new AlterTable('altered');
 
         // test for valid syntax, not valid engine semantics
         $id = new Column('id');
@@ -41,30 +38,36 @@ class AlterTableDecoratorTest extends \PHPUnit_Framework_TestCase
         $id->setOption('encrypted with', '(COLUMN_ENCRYPTION_KEY = key_name)');
         $id->setOption('masked with', "(FUNCTION = ' mask_function ')");
         $id->setOption('identity', '(1, 1)');
-        $at->addColumn($id);
+        $alterTable->addColumn($id);
 
-        $pk = new Column('named_pk');
-        $pk->addConstraint(new PrimaryKey(null, 'specified_pk'));
-        $at->addColumn($pk);
+        $primaryKey = new Column('named_pk');
+        $primaryKey->addConstraint(new PrimaryKey(null, 'specified_pk'));
+        $alterTable->addColumn($primaryKey);
 
         $this->assertEquals(
             "ALTER TABLE [altered]\n".
-                " ADD [id] INTEGER FILESTREAM COLLATE [Cyrillic_General_CI_AS] IDENTITY (1, 1) NOT NULL ROWGUIDCOL SPARSE ENCRYPTED WITH (COLUMN_ENCRYPTION_KEY = key_name) MASKED WITH (FUNCTION = ' mask_function ') PRIMARY KEY;\n".
+                " ADD [id] INTEGER FILESTREAM COLLATE [Cyrillic_General_CI_AS] IDENTITY (1, 1) NOT NULL " .
+                    "ROWGUIDCOL SPARSE ENCRYPTED WITH (COLUMN_ENCRYPTION_KEY = key_name) " .
+                    "MASKED WITH (FUNCTION = ' mask_function ') PRIMARY KEY;\n".
             "ALTER TABLE [altered]\n".
-                ' ADD [named_pk] INTEGER NOT NULL CONSTRAINT [specified_pk] PRIMARY KEY;',
-            $atd->setSubject($at)->getSqlString($platform)
+                " ADD [named_pk] INTEGER NOT NULL " .
+                    "CONSTRAINT [specified_pk] PRIMARY KEY;",
+            $alterDecorator->setSubject($alterTable)->getSqlString($platform)
         );
 
         // add constraint without columns
-        $at = new AlterTable('constrained');
-        $at->addConstraint(new PrimaryKey(['u_id', 'g_id'], 'UserGroup_PK'));
+        $alterTable = new AlterTable('constrained');
+        $alterTable->addConstraint(new PrimaryKey(['u_id', 'g_id'], 'UserGroup_PK'));
         $this->assertEquals(
             "ALTER TABLE [constrained]\n".
-                ' ADD CONSTRAINT [UserGroup_PK] PRIMARY KEY ([u_id], [g_id]);',
-            trim($atd->setSubject($at)->getSqlString($platform))
+                " ADD CONSTRAINT [UserGroup_PK] PRIMARY KEY ([u_id], [g_id]);",
+            trim($alterDecorator->setSubject($alterTable)->getSqlString($platform))
         );
     }
 
+    /**
+     * @covers Zend\Db\Sql\Platform\SqlServer\Ddl\AlterTableDecorator::getSqlString
+     */
     public function testIdentityBooleanConvertsToDefaultParams()
     {
         $platform = new TrustingSqlServerPlatform();
@@ -77,11 +80,14 @@ class AlterTableDecoratorTest extends \PHPUnit_Framework_TestCase
 
         $this->assertEquals(
             "ALTER TABLE [identifiable]\n".
-                ' ADD [id] INTEGER IDENTITY (1, 1) NOT NULL;',
+                " ADD [id] INTEGER IDENTITY (1, 1) NOT NULL;",
             $atd->setSubject($at)->getSqlString($platform)
         );
     }
 
+    /**
+     * @covers Zend\Db\Sql\Platform\SqlServer\Ddl\AlterTableDecorator::getSqlString
+     */
     public function testIdentityInvalidFormatThrowsException()
     {
         $platform = new TrustingSqlServerPlatform();
@@ -97,6 +103,9 @@ class AlterTableDecoratorTest extends \PHPUnit_Framework_TestCase
         $atd->setSubject($at)->getSqlString($platform);
     }
 
+    /**
+     * @covers Zend\Db\Sql\Platform\SqlServer\Ddl\AlterTableDecorator::getSqlString
+     */
     public function testVarbinarySyntaxCorrected()
     {
         $platform = new TrustingSqlServerPlatform();
@@ -107,7 +116,7 @@ class AlterTableDecoratorTest extends \PHPUnit_Framework_TestCase
         $at->addColumn(new Varbinary('binary'));
         $this->assertEquals(
             "ALTER TABLE [hasbinarydata]\n".
-                ' ADD [binary] VARBINARY (max) NOT NULL;',
+                " ADD [binary] VARBINARY (max) NOT NULL;",
             $atd->setSubject($at)->getSqlString($platform)
         );
     }
--- test/Sql/Platform/SqlServer/Ddl/CreateTableDecoratorTest.php
+++ test/Sql/Platform/SqlServer/Ddl/CreateTableDecoratorTest.php
@@ -1,12 +1,8 @@
 <?php
-
 /**
- * Zend Framework (http://framework.zend.com/).
- *
- * @link      http://github.com/zendframework/zf2 for the canonical source repository
- *
- * @copyright Copyright (c) 2005-2017 Zend Technologies USA Inc. (http://www.zend.com)
- * @license   http://framework.zend.com/license/new-bsd New BSD License
+ * @see       http://github.com/zendframework/zend-db for the canonical source repository
+ * @copyright Copyright (c) 2015-2017 Zend Technologies USA Inc. (http://www.zend.com)
+ * @license   http://github.com/zendframework/zend-db/blob/master/LICENSE.md New BSD License
  */
 
 namespace ZendTest\Db\Sql\Platform\SqlServer\Ddl;
@@ -27,23 +23,35 @@ class CreateTableDecoratorTest extends TestCase
     {
         $platform = new TrustingSqlServerPlatform();
 
-        $ctd = new CreateTableDecorator();
+        $createDecorator = new CreateTableDecorator();
 
-        $ct = new CreateTable('foo');
-        self::assertEquals("CREATE TABLE [foo] ( \n     \n)", $ctd->setSubject($ct)->getSqlString($platform));
+        $createTable = new CreateTable('foo');
+        self::assertEquals(
+            "CREATE TABLE [foo] ( \n     \n)",
+            $createDecorator->setSubject($createTable)->getSqlString($platform)
+        );
 
-        $ct = new CreateTable('foo', true);
-        self::assertEquals("CREATE TABLE [#foo] ( \n     \n)", $ctd->setSubject($ct)->getSqlString($platform));
+        $createTable = new CreateTable('foo', true);
+        self::assertEquals(
+            "CREATE TABLE [#foo] ( \n     \n)",
+            $createDecorator->setSubject($createTable)->getSqlString($platform)
+        );
 
-        $ct = new CreateTable('foo');
-        $ct->addColumn(new Column('bar'));
-        self::assertEquals("CREATE TABLE [foo] ( \n    [bar] INTEGER NOT NULL \n)", $ctd->setSubject($ct)->getSqlString($platform));
+        $createTable = new CreateTable('foo');
+        $createTable->addColumn(new Column('bar'));
+        self::assertEquals(
+            "CREATE TABLE [foo] ( \n    [bar] INTEGER NOT NULL \n)",
+            $createDecorator->setSubject($createTable)->getSqlString($platform)
+        );
 
-        $ct = new CreateTable('foo', true);
-        $ct->addColumn(new Column('bar'));
-        self::assertEquals("CREATE TABLE [#foo] ( \n    [bar] INTEGER NOT NULL \n)", $ctd->setSubject($ct)->getSqlString($platform));
+        $createTable = new CreateTable('foo', true);
+        $createTable->addColumn(new Column('bar'));
+        self::assertEquals(
+            "CREATE TABLE [#foo] ( \n    [bar] INTEGER NOT NULL \n)",
+            $createDecorator->setSubject($createTable)->getSqlString($platform)
+        );
 
-        $ct = new CreateTable('opinionated');
+        $createTable = new CreateTable('opinionated');
         // test for valid syntax, not valid engine semantics
         $id = new Column('id');
         $id->addConstraint(new PrimaryKey());
@@ -54,18 +62,21 @@ class CreateTableDecoratorTest extends TestCase
         $id->setOption('encrypted with', '(COLUMN_ENCRYPTION_KEY = key_name)');
         $id->setOption('masked with', "(FUNCTION = ' mask_function ')");
         $id->setOption('identity', '(1, 1)');
-        $ct->addColumn($id);
+        $createTable->addColumn($id);
 
-        $pk = new Column('named_pk');
-        $pk->addConstraint(new PrimaryKey(null, 'specified_pk'));
-        $ct->addColumn($pk);
+        $primaryKey = new Column('named_key');
+        $primaryKey->addConstraint(new PrimaryKey(null, 'specified_primary_key_name'));
+        $createTable->addColumn($primaryKey);
 
         self::assertEquals(
             "CREATE TABLE [opinionated] ( \n".
-            "    [id] INTEGER FILESTREAM COLLATE [Cyrillic_General_CI_AS] IDENTITY (1, 1) NOT NULL ROWGUIDCOL SPARSE ENCRYPTED WITH (COLUMN_ENCRYPTION_KEY = key_name) MASKED WITH (FUNCTION = ' mask_function ') PRIMARY KEY,\n".
-            "    [named_pk] INTEGER NOT NULL CONSTRAINT [specified_pk] PRIMARY KEY \n".
-            ')',
-            $ctd->setSubject($ct)->getSqlString($platform)
+            "    [id] INTEGER FILESTREAM COLLATE [Cyrillic_General_CI_AS] IDENTITY (1, 1) NOT NULL " .
+                    "ROWGUIDCOL SPARSE ENCRYPTED WITH (COLUMN_ENCRYPTION_KEY = key_name) " .
+                    "MASKED WITH (FUNCTION = ' mask_function ') ".
+                    "PRIMARY KEY,\n".
+            "    [named_key] INTEGER NOT NULL CONSTRAINT [specified_primary_key_name] PRIMARY KEY \n".
+            ")",
+            $createDecorator->setSubject($createTable)->getSqlString($platform)
         );
     }
 }
