229443eb0df21854aca96cb44d7bc50c2a807e0f
Sasha Alex Romanenko <alex@alex-tech-adventures.com>
2017-03-10 19:46:34 -0500

    #215 AlterTable should not be abbreviated `ct`

--- test/Sql/Platform/SqlServer/Ddl/AlterTableDecoratorTest.php
+++ test/Sql/Platform/SqlServer/Ddl/AlterTableDecoratorTest.php
@@ -28,8 +28,8 @@ class AlterTableDecoratorTest extends \PHPUnit_Framework_TestCase
     {
         $platform = new TrustingSqlServerPlatform();
 
-        $ctd = new AlterTableDecorator();
-        $ct = new AlterTable('altered');
+        $atd = new AlterTableDecorator();
+        $at = new AlterTable('altered');
 
         // test for valid syntax, not valid engine semantics
         $id = new Column('id');
@@ -41,27 +41,27 @@ class AlterTableDecoratorTest extends \PHPUnit_Framework_TestCase
         $id->setOption('encrypted with', '(COLUMN_ENCRYPTION_KEY = key_name)');
         $id->setOption('masked with', "(FUNCTION = ' mask_function ')");
         $id->setOption('identity', '(1, 1)');
-        $ct->addColumn($id);
+        $at->addColumn($id);
 
         $pk = new Column('named_pk');
         $pk->addConstraint(new PrimaryKey(null, 'specified_pk'));
-        $ct->addColumn($pk);
+        $at->addColumn($pk);
 
         $this->assertEquals(
             "ALTER TABLE [altered]\n".
                 " ADD [id] INTEGER FILESTREAM COLLATE [Cyrillic_General_CI_AS] IDENTITY (1, 1) NOT NULL ROWGUIDCOL SPARSE ENCRYPTED WITH (COLUMN_ENCRYPTION_KEY = key_name) MASKED WITH (FUNCTION = ' mask_function ') PRIMARY KEY;\n".
             "ALTER TABLE [altered]\n".
-                " ADD [named_pk] INTEGER NOT NULL CONSTRAINT [specified_pk] PRIMARY KEY;",
-            $ctd->setSubject($ct)->getSqlString($platform)
+                ' ADD [named_pk] INTEGER NOT NULL CONSTRAINT [specified_pk] PRIMARY KEY;',
+            $atd->setSubject($at)->getSqlString($platform)
         );
 
         // add constraint without columns
-        $ct = new AlterTable('constrained');
-        $ct->addConstraint(new PrimaryKey(['u_id', 'g_id'], 'UserGroup_PK'));
+        $at = new AlterTable('constrained');
+        $at->addConstraint(new PrimaryKey(['u_id', 'g_id'], 'UserGroup_PK'));
         $this->assertEquals(
             "ALTER TABLE [constrained]\n".
                 ' ADD CONSTRAINT [UserGroup_PK] PRIMARY KEY ([u_id], [g_id]);',
-            trim($ctd->setSubject($ct)->getSqlString($platform))
+            trim($atd->setSubject($at)->getSqlString($platform))
         );
     }
 
@@ -69,16 +69,16 @@ class AlterTableDecoratorTest extends \PHPUnit_Framework_TestCase
     {
         $platform = new TrustingSqlServerPlatform();
 
-        $ctd = new AlterTableDecorator();
-        $ct = new AlterTable('identifiable');
+        $atd = new AlterTableDecorator();
+        $at = new AlterTable('identifiable');
         $id = new Column('id');
         $id->setOption('identity', true);
-        $ct->addColumn($id);
+        $at->addColumn($id);
 
         $this->assertEquals(
             "ALTER TABLE [identifiable]\n".
                 ' ADD [id] INTEGER IDENTITY (1, 1) NOT NULL;',
-            $ctd->setSubject($ct)->getSqlString($platform)
+            $atd->setSubject($at)->getSqlString($platform)
         );
     }
 
@@ -86,29 +86,29 @@ class AlterTableDecoratorTest extends \PHPUnit_Framework_TestCase
     {
         $platform = new TrustingSqlServerPlatform();
 
-        $ctd = new AlterTableDecorator();
-        $ct = new AlterTable('invalid');
+        $atd = new AlterTableDecorator();
+        $at = new AlterTable('invalid');
 
         $id = new Column('id');
         $id->setOption('identity', '1');
-        $ct->addColumn($id);
+        $at->addColumn($id);
 
         $this->setExpectedException(InvalidArgumentException::class);
-        $ctd->setSubject($ct)->getSqlString($platform);
+        $atd->setSubject($at)->getSqlString($platform);
     }
 
     public function testVarbinarySyntaxCorrected()
     {
         $platform = new TrustingSqlServerPlatform();
 
-        $ctd = new AlterTableDecorator();
-        $ct = new AlterTable('hasbinarydata');
+        $atd = new AlterTableDecorator();
+        $at = new AlterTable('hasbinarydata');
 
-        $ct->addColumn(new Varbinary('binary'));
+        $at->addColumn(new Varbinary('binary'));
         $this->assertEquals(
             "ALTER TABLE [hasbinarydata]\n".
                 ' ADD [binary] VARBINARY (max) NOT NULL;',
-            $ctd->setSubject($ct)->getSqlString($platform)
+            $atd->setSubject($at)->getSqlString($platform)
         );
     }
 }
