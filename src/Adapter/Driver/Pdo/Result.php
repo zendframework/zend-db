8fe1def21d7d1ac63f80d2006f5c316cb87bfd12
Chris <c.kruining@fyn.nl>
2017-12-22 15:19:16 +0100

    whoops, I weirded out on the condition...

--- src/Adapter/Driver/Pdo/Result.php
+++ src/Adapter/Driver/Pdo/Result.php
@@ -107,7 +107,7 @@ class Result implements Iterator, ResultInterface
      */
     public function setFetchMode($fetchMode)
     {
-        if (! (($fetchMode >= 1 && $fetchMode <= 12) || $fetchMode === \PDO::FETCH_CLASS | \PDO::FETCH_PROPS_LATE)) {
+        if (($fetchMode < 1 || $fetchMode > 12) && $fetchMode !== \PDO::FETCH_CLASS | \PDO::FETCH_PROPS_LATE) {
             throw new Exception\InvalidArgumentException(
                 'The fetch mode must be one of the PDO::FETCH_* constants.'
             );
