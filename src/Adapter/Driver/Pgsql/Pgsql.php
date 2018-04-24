60878db9fb520d29576f7b54fc831f337b6a9f85
Stalin <tp.trix@gmail.com>
2018-04-25 01:48:36 +0300

    Add is_array check for empty extra options.

--- src/Adapter/Driver/Pgsql/Pgsql.php
+++ src/Adapter/Driver/Pgsql/Pgsql.php
@@ -61,7 +61,9 @@ class Pgsql implements DriverInterface, Profiler\ProfilerAwareInterface
             $connection = new Connection($connection);
         }
 
-        $this->options = array_merge($this->options, $options);
+        if (is_array($options)) {
+            $this->options = array_merge($this->options, $options);
+        }
         $this->registerConnection($connection);
         $this->registerStatementPrototype(($statementPrototype) ?: new Statement());
         $this->registerResultPrototype(($resultPrototype) ?: new Result());
