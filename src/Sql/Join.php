b9ce140a3a1692d5d1035061ebd6786f53cb187f
Andi Rückauer <arueckauer@gmail.com>
2019-03-16 21:06:19 +0100

    Updated file header comment, added strict_types declaration

--- src/Sql/Join.php
+++ src/Sql/Join.php
@@ -1,9 +1,11 @@
 <?php
 /**
- * @link      http://github.com/zendframework/zend-db for the canonical source repository
- * @copyright Copyright (c) 2005-2016 Zend Technologies USA Inc. (http://www.zend.com)
- * @license   http://framework.zend.com/license/new-bsd New BSD License
+ * @see       https://github.com/zendframework/zend-db for the canonical source repository
+ * @copyright Copyright (c) 2005-2019 Zend Technologies USA Inc. (https://www.zend.com)
+ * @license   https://github.com/zendframework/zend-db/blob/master/LICENSE.md New BSD License
  */
+
+declare(strict_types=1);
 
 namespace Zend\Db\Sql;
 
