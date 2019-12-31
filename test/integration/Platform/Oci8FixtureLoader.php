fdc98c8f75f04e922d9d20edabd25fb256119663
Michał Bundyra <contact@webimpress.com>
2019-12-31 12:26:20 -0600

    Updates fixture loader for Oci8

--- test/integration/Platform/Oci8FixtureLoader.php
+++ test/integration/Platform/Oci8FixtureLoader.php
@@ -1,58 +1,37 @@
 <?php
 /**
- * Zend Framework (http://framework.zend.com/)
- *
- * @link      http://github.com/zendframework/zf2 for the canonical source repository
- * @copyright Copyright (c) 2018 Zend Technologies USA Inc. (http://www.zend.com)
- * @license   http://framework.zend.com/license/new-bsd New BSD License
+ * @see       https://github.com/zendframework/zend-db for the canonical source repository
+ * @copyright Copyright (c) 2019 Zend Technologies USA Inc. (https://www.zend.com)
+ * @license   https://github.com/zendframework/zend-db/blob/master/LICENSE.md New BSD License
  */
 
 namespace ZendIntegrationTest\Db\Platform;
 
+use RuntimeException;
+
 class Oci8FixtureLoader implements FixtureLoader
 {
-    
+    /** @var string */
     private $fixtureFile = __DIR__ . '/../TestFixtures/oci8.sql';
-    
+
+    /** @var resource */
     private $oci8;
+
+    /** @var bool */
     private $initialRun = true;
     
     public function createDatabase()
     {
-        /*var_dump(getenv('TESTS_ZEND_DB_ADAPTER_DRIVER_OCI8_USERNAME'));
-        var_dump(getenv('TESTS_ZEND_DB_ADAPTER_DRIVER_OCI8_PASSWORD'));
-        var_dump(getenv('TESTS_ZEND_DB_ADAPTER_DRIVER_OCI8_CONNECTIONSTRING'));
-        var_dump(getenv('TESTS_ZEND_DB_ADAPTER_DRIVER_OCI8_CHARSET'));
-        die();*/
         $this->oci8 = oci_pconnect(
             getenv('TESTS_ZEND_DB_ADAPTER_DRIVER_OCI8_USERNAME'),
             getenv('TESTS_ZEND_DB_ADAPTER_DRIVER_OCI8_PASSWORD'),
             getenv('TESTS_ZEND_DB_ADAPTER_DRIVER_OCI8_CONNECTIONSTRING'),
             getenv('TESTS_ZEND_DB_ADAPTER_DRIVER_OCI8_CHARSET'),
-            null);
-        
+            null
+        );
         
         $this->dropDatabase();
-        /*if (false === $this->oci8->exec(sprintf(
-            "CREATE DATABASE %s",
-            getenv('TESTS_ZEND_DB_ADAPTER_DRIVER_OCI8_DATABASE')
-        ))) {
-            throw new \Exception(sprintf(
-                "I cannot create the Oci8 %s test database: %s",
-                getenv('TESTS_ZEND_DB_ADAPTER_DRIVER_OCI8_DATABASE'),
-                print_r(oci_error($this->oci8), true)
-            ));
-        }*/
-        
-        
-        /*unset($this->oci8);
-        $this->oci8 = oci_connect(
-            getenv('TESTS_ZEND_DB_ADAPTER_DRIVER_OCI8_USERNAME'),
-            getenv('TESTS_ZEND_DB_ADAPTER_DRIVER_OCI8_PASSWORD'),
-            getenv('TESTS_ZEND_DB_ADAPTER_DRIVER_OCI8_CONNECTIONSTRING'),
-            getenv('TESTS_ZEND_DB_ADAPTER_DRIVER_OCI8_CHARSET'),
-            null);*/
-        
+
         $sql = file_get_contents($this->fixtureFile);
         $sql2 = <<<SQL
         $sql
@@ -61,8 +40,8 @@ SQL;
         $resource = oci_parse($this->oci8, $sql2);
         $ret = oci_execute($resource);
         if (false === $ret) {
-            throw new \Exception(sprintf(
-                "I cannot create the table for database %s. Check the %s file. %s ",
+            throw new RuntimeException(sprintf(
+                'I cannot create the table for database %s. Check the %s file. %s',
                 getenv('TESTS_ZEND_DB_ADAPTER_DRIVER_OCI8_DATABASE'),
                 $this->fixtureFile,
                 print_r(oci_error($this->oci8), true)
